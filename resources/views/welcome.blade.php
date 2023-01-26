5df8783de7d60eb1819e4cae70a61f2168f77929
Andrey Volynkin <kazakmt4@gmail.com>
2023-01-26 10:35:15 +0300

    test commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>4.6 Практическая работа</title>
+
+</head>
+<body>
+<div>
+
+    <div>
+        Laravel ver{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+    </div>
+
+</div>
+</body>
+</html>
